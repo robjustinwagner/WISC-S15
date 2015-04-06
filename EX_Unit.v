dfcb252d35dfe9c30f6a4a182c046687b87889fc
Robert Wagner <rob.justin.wagner@gmail.com>
2015-04-06 15:31:02 -0500

    correct sra,srl,sll

--- EX_Unit.v
+++ EX_Unit.v
@@ -9,7 +9,7 @@ input clk;
 
 
 //MODULE INSTANTIATIONS
-ALU alu(data_one, data_two, control, zero, result, flags);		//FIX THIS
+ALU alu(data_one, data_two, shift, load_half_imm, control, zero, result, flags);		//FIX THIS
 Forwarding_Unit FU();																				//FIX THIS
 
 
