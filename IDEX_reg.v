b8ffadca7562854b1b018e40a0621b5e4aa61e65
gnygard <gnygard@wisc.edu>
2015-04-07 12:16:17 -0500

    First 2 pipeline registers pretty much done

--- IDEX_reg.v
+++ IDEX_reg.v
@@ -1,53 +1,73 @@
 // Author: Graham Nygard, Robert Wagner
 
 module IDEX_reg(clk, 
-	pc_in, mem_to_reg_in, reg_to_mem_in, alu_op_in, alu_src_in, shift_in, sign_ext_in, 
-		load_half_imm_in, branch_in, rd_data_0_in, rd_data_1_in, call_in,
-	pc_out, mem_to_reg_out, reg_to_mem_out, alu_op_out, alu_src_out, shift_out, sign_ext_out, 
-		load_half_imm_out, branch_out, rd_data_0_out, rd_data_1_out, call_out)
+	PC_in, mem_to_reg_in, reg_to_mem_in, alu_op_in, alu_src_in, shift_in, sign_ext_in, 
+		load_half_imm_in, branch_in, rd_data_1_in, rd_data_2_in, call_in, reg_rd_in,
+	PC_out, mem_to_reg_out, reg_to_mem_out, alu_op_out, alu_src_out, shift_out, sign_ext_out, 
+		load_half_imm_out, branch_out, rd_data_1_out, rd_data_2_out, call_out, reg_rd_out);
 
-//INPUTS
-input 		clk;
-input 	[15:0] 	pc_in;
-input 		mem_to_reg_in;
-input		reg_to_mem_in;
-input	[2:0]	alu_op_in;
-input		alu_src_in;
-input 	[3:0] 	shift_in;
-input	[15:0]	sign_ext_in;
-input 	[7:0] 	load_half_imm_in;
-input	[3:0]	branch_in;
-input 	[15:0] 	rd_data_0_in;
-input 	[15:0] 	rd_data_1_in;
-input	[11:0]	call_in;
+//////////////////////////INPUTS/////////////////////////////
 
-//OUTPUTS
-output 	[15:0] 	pc_out;
-input 		mem_to_reg_out;
-input		reg_to_mem_out;
-input	[2:0]	alu_op_out;
-input		alu_src_out;
-input 	[3:0] 	shift_out;
-input	[15:0]	sign_ext_out;
-input 	[7:0] 	load_half_imm_out;
-input	[3:0]	branch_out;
-input 	[15:0] 	rd_data_0_out;
-input 	[15:0] 	rd_data_1_out;
-input	[11:0]	call_out;
+input        clk;
+
+input        mem_to_reg_in;    // LW signal to Memory unit 
+input	       reg_to_mem_in;    // SW signal to Memory unit 
+
+input	[2:0]	 branch_in;        // Branch condition
+
+input		      alu_src_in;       // ALU operand source seleciton
+
+input	[2:0]	 alu_op_in;        // Desired ALU operation
+input [3:0]  shift_in;         // Imm of Arithmetic Inst
+input [7:0]  load_half_imm_in; // Imm of Load/Save Inst
+input [15:0] rd_data_1_in;     // Regfile Read_Bus_1
+input [15:0] rd_data_2_in;     // Regfile Read_Bus_2
+input	[15:0]	sign_ext_in;      // Output of sign_ext unit
+
+input [3:0]  reg_rd_in;        // Future Regfile dest
+input	[11:0]	call_in;          // Call target
+input [15:0] PC_in;            // PC for branch/call/ret
+
+/////////////////////////END INPUTS///////////////////////////
+
+//////////////////////////OUTPUTS/////////////////////////////
+
+output logic        mem_to_reg_out;    // LW signal to Memory unit 
+output	logic        reg_to_mem_out;    // SW signal to Memory unit 
+
+output	logic [2:0]	 branch_out;        // Branch condition
+
+output	logic	       alu_src_out;       // ALU operand 2 seleciton
+
+output	logic [2:0]	 alu_op_out;        // ALU operation
+output logic [3:0]  shift_out;         // ALU shift input
+output logic [7:0]  load_half_imm_out; // ALU imm load input
+output logic [15:0] rd_data_1_out;     // ALU operand 1
+output logic [15:0] rd_data_2_out;     // ALU operand 2
+output	logic [15:0]	sign_ext_out;      // ALU operand 2
+
+output logic [3:0]  reg_rd_out;        // Future Regfile dest
+output	logic [11:0]	call_out;          // Call target
+output logic [15:0] PC_out;            // PC for branch/call/ret
+
+////////////////////////END OUTPUTS///////////////////////////
 
 always @(posedge clk) begin
-	pc_out <= pc_in;
-	mem_to_reg_out <= mem_to_reg_in;
-	reg_to_mem_out <= reg_to_mem_in;
-	alu_op_out <= alu_op_in;
-	alu_src_out <= alu_src_in;
-	shift_out <= shift_in;
-	sign_ext_out <= sign_ext_in;
+	
+	// Pass on all inputs to next section of the pipe
+	PC_out            <= PC_in;
+	mem_to_reg_out    <= mem_to_reg_in;
+	reg_to_mem_out    <= reg_to_mem_in;
+	alu_op_out        <= alu_op_in;
+	alu_src_out       <= alu_src_in;
+	shift_out         <= shift_in;
+	sign_ext_out      <= sign_ext_in;
 	load_half_imm_out <= load_half_imm_in;
-	branch_out <= branch_in;
-	rd_data_0_out <= rd_data_0_in;
-	rd_data_1_out <= rd_data_1_in;
-	call_out <= call_in;
+	branch_out        <= branch_in;
+	rd_data_1_out     <= rd_data_1_in;
+	rd_data_2_out     <= rd_data_2_in;
+	call_out          <= call_in;
+	
 end
 
 endmodule
--- IFID_reg.v
+++ IFID_reg.v
@@ -19,20 +19,10 @@ output logic [7:0]  load_save_imm; // Inst[7:0]   - Imm of Load/Save Inst
 output logic [11:0] call;          // Inst[11:0]  - Call target
 output logic [15:0] PC_out;        // Program counter
 
-//INTERNAL CONTROL
-logic [3:0]  CI;
-logic [3:0]  RS;
-logic [3:0]  RT;
-logic [3:0]  RD;
-logic [3:0]  AI;
-logic [7:0]  LSI;
-logic [11:0] CALL;
-logic [15:0] PC;
-
 //NO OPERATION FOR PIPE STALL
 logic [15:0] NO_OP = 16'hF000;
 
-                                           /* YANK THIS OUT TO SEPARATE MODULE
+                            /* YANK THIS OUT TO SEPARATE MODULE
 //PIPELINE ENABLE COUNTER
 logic [1:0] counter;
 logic rst_counter;
@@ -43,9 +33,9 @@ always @(posedge clk) begin
    counter <= counter + 1;
 end
 
-//ENABLE THE PIPELINE WHEN COUNTER MAXES OUT
+//ENABLE THE PIPELINE
 assign enable = &counter;
-                                             END OF THE YANK */
+                                END OF THE YANK */
 
 // Pipeline register will be sensitive flopped clock
 always @(posedge clk) begin
@@ -54,23 +44,22 @@ always @(posedge clk) begin
    if (!hazard) begin
    
       // Pass on the PC
-      PC <= PC_in;
+      PC_out        <= PC_in;
     
       // Set the input to the control unit
-	   //cntrl_input <= instruction[15:12];
-	   CI <= instruction[15:12];
+	   cntrl_input   <= instruction[15:12];
 
       // Specify the src and dest registers
-	   RS <= instruction[7:4];
-	   RT <= instruction[3:0];
-	   RD <= instruction[11:8];
+	   reg_rs        <= instruction[7:4];
+	   reg_rt        <= instruction[3:0];
+	   reg_rd        <= instruction[11:8];
 	
 	   // Set the immediate fields of instruction
-	   AI  <= instruction[3:0];
-	   LSI <= instruction[7:0];
+	   arith_imm     <= instruction[3:0];
+	   load_save_imm <= instruction[7:0];
 	
 	   // Set call target
-	   CALL <= instruction[11:0];
+	   call          <= instruction[11:0];
 	   
 	end
 	
@@ -78,36 +67,25 @@ always @(posedge clk) begin
 	else begin
 	    
 	   // Lock the PC
-      PC <= 16'hzzzz;
+      PC_out        <= 16'hzzzz;
     
       // Set the input to the control unit
-	   CI <= NO_OP[15:12];
+	   cntrl_input   <= NO_OP[15:12];
 
       // Specify the src and dest registers
-	   RS <= NO_OP[7:4];
-	   RT <= NO_OP[3:0];
-	   RD <= NO_OP[11:8];
+	   reg_rs        <= NO_OP[7:4];
+	   reg_rt        <= NO_OP[3:0];
+	   reg_rd        <= NO_OP[11:8];
 	
 	   // Set the immediate fields of instruction
-	   AI  <= NO_OP[3:0];
-	   LSI <= NO_OP[7:0];
+	   arith_imm     <= NO_OP[3:0];
+	   load_save_imm <= NO_OP[7:0];
 	
 	   // Set call target
-	   CALL <= NO_OP[11:0];
+	   call          <= NO_OP[11:0];
 	   
 	end
 	
 end
-
-//ASSIGN INTERNAL CONTROLS TO OUTPUT
-assign cntrl_input    = CI;
-assign reg_rs         = RS;
-assign reg_rt         = RT;
-assign reg_rd         = RD;
-assign arith_imm      = AI;
-assign load_save_imm  = LSI;
-assign call           = CALL;
-assign PC_out         = PC;
-
 
 endmodule
