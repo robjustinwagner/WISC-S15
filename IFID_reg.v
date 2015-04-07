b47334aeb2f93c23d17f17e42b2a0effb3e18a0b
gnygard <gnygard@wisc.edu>
2015-04-07 11:24:09 -0500

    Added hazard handling
    The pipe will be stalled and a NO_OP instruction will be passed down the
    pipe when a data hazard is detected. This detection is not yet
    implemented.

--- IFID_reg.v
+++ IFID_reg.v
@@ -1,22 +1,23 @@
 // Author: Graham Nygard, Robert Wagner
 
-module IFID_reg(clk, PC_in, instruction, reg_rs, reg_rt, reg_rd,
+module IFID_reg(clk, hazard, PC_in, instruction, reg_rs, reg_rt, reg_rd,
                 cntrl_input, arith_imm, load_save_imm, call, PC_out);
 
 //INPUTS
-input clk;
+input        clk;
+input        hazard;        // Stall the pipe for hazards
 input [15:0] instruction;   // Instruction to execute
 input [15:0] PC_in;         // Program counter
 
 //OUTPUTS
-output [3:0]  cntrl_input;   // Inst[15:12] - Opcode
-output [3:0]  reg_rs;        // Inst[7:4]   - Register rs
-output [3:0]  reg_rt;        // Inst[3:0]   - Register rt
-output [3:0]  reg_rd;        // Inst[11:8]  - Register rd
-output [3:0]  arith_imm;     // Inst[3:0]   - Imm of Arithmetic Inst
-output [7:0]  load_save_imm; // Inst[7:0]   - Imm of Load/Save Inst
-output [11:0] call;          // Inst[11:0]  - Call target
-output [15:0] PC_out;        // Program counter
+output logic [3:0]  cntrl_input;   // Inst[15:12] - Opcode
+output logic [3:0]  reg_rs;        // Inst[7:4]   - Register rs
+output logic [3:0]  reg_rt;        // Inst[3:0]   - Register rt
+output logic [3:0]  reg_rd;        // Inst[11:8]  - Register rd
+output logic [3:0]  arith_imm;     // Inst[3:0]   - Imm of Arithmetic Inst
+output logic [7:0]  load_save_imm; // Inst[7:0]   - Imm of Load/Save Inst
+output logic [11:0] call;          // Inst[11:0]  - Call target
+output logic [15:0] PC_out;        // Program counter
 
 //INTERNAL CONTROL
 logic [3:0]  CI;
@@ -28,7 +29,10 @@ logic [7:0]  LSI;
 logic [11:0] CALL;
 logic [15:0] PC;
 
-/* YANK THIS OUT TO SEPARATE MODULE */
+//NO OPERATION FOR PIPE STALL
+logic [15:0] NO_OP = 16'hF000;
+
+                                           /* YANK THIS OUT TO SEPARATE MODULE
 //PIPELINE ENABLE COUNTER
 logic [1:0] counter;
 logic rst_counter;
@@ -41,46 +45,69 @@ end
 
 //ENABLE THE PIPELINE WHEN COUNTER MAXES OUT
 assign enable = &counter;
-/* END OF THE YANK */
+                                             END OF THE YANK */
 
 // Pipeline register will be sensitive flopped clock
-always @(posedge enable) begin
+always @(posedge clk) begin
     
-   // Pass on the PC
-   PC <= PC_in;
+   // Don't stall the pipe
+   if (!hazard) begin
+   
+      // Pass on the PC
+      PC <= PC_in;
     
-   // Set the input to the control unit
-	//cntrl_input <= instruction[15:12];
-	CI <= instruction[15:12];
+      // Set the input to the control unit
+	   //cntrl_input <= instruction[15:12];
+	   CI <= instruction[15:12];
 
-   // Specify the src and dest registers
-   //reg_rs <= instruction[7:4];
-	//reg_rt <= instruction[3:0];
-	//reg_rd <= instruction[11:8];
-	RS <= instruction[7:4];
-	RT <= instruction[3:0];
-	RD <= instruction[11:8];
+      // Specify the src and dest registers
+	   RS <= instruction[7:4];
+	   RT <= instruction[3:0];
+	   RD <= instruction[11:8];
 	
-	// Set the immediate fields of instruction
-	//arith_imm <= instruction[3:0];
-	//load_save_imm <= instruction[7:0];
-	AI  <= instruction[3:0];
-	LSI <= instruction[7:0];
+	   // Set the immediate fields of instruction
+	   AI  <= instruction[3:0];
+	   LSI <= instruction[7:0];
 	
-	// Set call target
-	CALL <= instruction[11:0];
+	   // Set call target
+	   CALL <= instruction[11:0];
+	   
+	end
+	
+	// Stall the pipe
+	else begin
+	    
+	   // Lock the PC
+      PC <= 16'hzzzz;
+    
+      // Set the input to the control unit
+	   CI <= NO_OP[15:12];
+
+      // Specify the src and dest registers
+	   RS <= NO_OP[7:4];
+	   RT <= NO_OP[3:0];
+	   RD <= NO_OP[11:8];
+	
+	   // Set the immediate fields of instruction
+	   AI  <= NO_OP[3:0];
+	   LSI <= NO_OP[7:0];
+	
+	   // Set call target
+	   CALL <= NO_OP[11:0];
+	   
+	end
 	
 end
 
 //ASSIGN INTERNAL CONTROLS TO OUTPUT
-assign cntrl_input   = CI;
-assign reg_rs        = RS;
-assign reg_rt        = RT;
-assign reg_rd        = RD;
-assign arith_imm     = AI;
-assign load_save_imm = LSI;
-assign call          = CALL;
-assign PC_in         = PC;
+assign cntrl_input    = CI;
+assign reg_rs         = RS;
+assign reg_rt         = RT;
+assign reg_rd         = RD;
+assign arith_imm      = AI;
+assign load_save_imm  = LSI;
+assign call           = CALL;
+assign PC_out         = PC;
 
 
 endmodule
--- IF_Unit.v
+++ IF_Unit.v
@@ -1,44 +1,47 @@
 // Author: Graham Nygard, Robert Wagner
 
-module IF_Unit(clk, PC_src, PC_branch, read_cntrl,
+module IF_Unit(clk, PC_src, PC_branch, hazard,
                PC_out, instruction);
 
 //INPUTS
 input        clk;
-input        PC_src;
-input        read_cntrl;
+input        PC_src;        // Mux select for choosing PC source
+input        hazard;        // Disable PC update for hazards
 input [15:0] PC_branch;
 
 //OUTPUTS
 output logic [15:0] PC_out;
-output [15:0] instruction;
+output logic [15:0] instruction;
 
 //INTERNAL CONTROL
+logic [15:0] PC_plus_4;
 logic [15:0] PC_update;
 logic [15:0] PC_address;
 
 //MODULE INSTANTIATIONS
 
 // PC register
-Reg_16bit PC(.clk(clk), .en(1), .d(PC_update), .q(PC_address));
+Reg_16bit PC(.clk(clk), .en(!hazard), .d(PC_update), .q(PC_address));
 
 // Instruction cache
 Instruction_Memory instr_mem(.clk(clk), .addr(PC_address),
-                             .instr(instruction), .rd_en(read_cntrl));
+                             .instr(instruction), .rd_en(!hazard));
 
 //PC update logic (branch target or next instr)
 always_comb begin
+    
+    PC_plus_4 = PC_address + 4;
     
     if (PC_src) begin
         PC_update = PC_branch;
     end
     
     else begin
-        PC_update = PC_address + 4;
+        PC_update = PC_plus_4;
     end
     
 end
 
-assign PC_out = PC_update;
+assign PC_out = PC_plus_4;
 
 endmodule
